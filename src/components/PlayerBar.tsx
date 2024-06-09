8b4ab958b53f9695d8df8a23981c60a3c8c9605e
Vitor Butkus <vitorbutkus@gmail.com>
2024-06-09 12:28:01 -0300

    change song in PLayerBar to Sky Fits Heaven

--- src/components/PlayerBar.tsx
+++ src/components/PlayerBar.tsx
@@ -18,14 +18,14 @@ export default function PlayerBar() {
     player = new Player({
       container: playerRef.current,
       audio: {
-        title: 'Nothing Really Matters (demo)',
+        title: 'Sky Fits Heaven (demo)',
         artist: 'Madonna',
         // cover: '/images/couple-pizza.png',
         cover:
           'https://1.bp.blogspot.com/-LoKlow8dGZc/XQ9jIeR9DxI/AAAAAAABGVc/EKlE83PyBm8owjwVuF9ywBE5SS2xZjJGgCLcBGAs/s1600/Ray%2Bof%2BLight%2BDemo%2BAssembly%2Bby%2Balb69.jpg',
         // src: '/audio/audio.mp3',
         // src: 'https://s3i4vqew3cvrd6rabrqoewqqruwkhspfzca532jrm7wx6x5e7sja.arweave.net/ltHKwJbYqxH6IAxg4loQjSyjyeXIgd3pMWftf1-k_JI',
-        src: 'https://archive.org/download/madonna-rol-demos/08%20Nothing%20Really%20Matters.mp3',
+        src: 'https://ia904507.us.archive.org/16/items/madonna-rol-demos/01%20Sky%20Fits%20Heaven.mp3',
       },
       download: true,
     });
