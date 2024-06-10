73687410a9017fbcb6eb1544ddfd2f2d029b4cb9
Vitor Butkus <vitorbutkus@gmail.com>
2024-06-10 19:46:58 -0300

    add enough padding-bottom to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
     <footer className='bg-neutral-50 border-t border-neutral-200'>
-      <div className='max-w-6xl mx-auto px-5 p-10'>Footer</div>
+      <div className='max-w-6xl mx-auto px-5 pt-10 pb-[140px] md:pb-[124px]'>
+        Footer
+      </div>
     </footer>
   );
 };
