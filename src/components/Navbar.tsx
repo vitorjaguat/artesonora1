d607779a81ad713c5e2ac51153f9892b06e8d4ca
Vitor Butkus <vitorbutkus@gmail.com>
2024-06-11 19:24:34 -0300

    setting Navbar animations

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,119 @@
 import Link from 'next/link';
 import NavbarMobile from './NavbarMobile';
+import { useAnimationControls, motion } from 'framer-motion';
+import { useState } from 'react';
+
+interface SubMenuData {
+  name: string;
+  link: string;
+}
+
+const subMenuDataManager = {
+  sobre: [
+    {
+      name: 'Sobre o Arte Sonora',
+      link: '/sobre',
+    },
+    {
+      name: 'Quem somos',
+      link: '/sobre#quem-somos',
+    },
+    {
+      name: 'Uma breve história',
+      link: '/sobre#breve-historia',
+    },
+    {
+      name: 'Colaboradores',
+      link: '/sobre#colaboradores',
+    },
+    {
+      name: 'Links',
+      link: '/sobre#links',
+    },
+  ],
+  programas: [
+    {
+      name: 'Podcast',
+      link: '/podcast',
+    },
+    {
+      name: 'Mixtape',
+      link: '/mixtape',
+    },
+    {
+      name: 'Na História',
+      link: '/na-historia',
+    },
+  ],
+};
 
 export default function Navbar() {
+  const controls = useAnimationControls();
+  const [subMenuData, setSubMenuData] = useState<SubMenuData[] | null>(null);
+
+  const handleHover = (data: any) => {
+    setSubMenuData(data);
+    controls.start('on');
+  };
   return (
     <>
       {/* DESKTOP NAV */}
-      <div
-        className='hidden md:flex fixed top-0 left-0 w-20 h-[calc(100vh-84px)] bg-black z-1 p-2 items-center justify-around gap-4 uppercase'
-        style={{ writingMode: 'vertical-rl' }}
-      >
-        <Link href='/'>
-          <div className='text-zinc-500 duration-300 hover:text-white rotate-180'>
-            home
-          </div>
-        </Link>
-        <Link href='/sobre'>
-          <div className=' text-zinc-500 duration-300 hover:text-white rotate-180'>
+      <div className='hidden md:inline fixed top-0 left-0 w-20 h-screen bg-black z-1'>
+        <div
+          className='md:flex  w-20 h-[calc(100vh-84px)] items-center justify-around gap-4 uppercase'
+          style={{ writingMode: 'vertical-rl' }}
+        >
+          <Link href='/'>
+            <div className='w-full text-zinc-500 duration-300 hover:text-white rotate-180'>
+              home
+            </div>
+          </Link>
+
+          <div
+            className='w-full flex items-center text-zinc-500 duration-300 hover:text-white rotate-180 cursor-pointer'
+            onMouseEnter={() => handleHover(subMenuDataManager.sobre)}
+            // onMouseLeave={() => controls.start('off')}
+          >
             sobre
           </div>
-        </Link>
-        <div className=' text-zinc-500 duration-300 hover:text-white rotate-180'>
-          programas
-        </div>
-        <div className='text-zinc-500 duration-300 hover:text-white rotate-180'>
-          arquivos
-        </div>
-        <div className='text-zinc-500 duration-300 hover:text-white rotate-180'>
-          contato
+
+          <div
+            className='w-full flex items-center  text-zinc-500 duration-300 hover:text-white rotate-180 cursor-pointer'
+            onMouseEnter={() => handleHover(subMenuDataManager.programas)}
+            // onMouseLeave={() => controls.start('off')}
+          >
+            programas
+          </div>
+          <div className='text-zinc-500 duration-300 hover:text-white rotate-180 cursor-pointer'>
+            arquivos
+          </div>
+          <div className='text-zinc-500 duration-300 hover:text-white rotate-180'>
+            contato
+          </div>
         </div>
       </div>
+      {/* SUBMENU */}
+      <motion.div
+        className='hidden fixed top-0 left-20 bg-black/80 h-[calc(100vh-84px)] items-center justify-around gap-4 uppercase'
+        onMouseEnter={() => controls.start('on')}
+        onMouseLeave={() => controls.start('off')}
+        style={{ writingMode: 'vertical-rl' }}
+        initial='off'
+        animate={controls}
+        variants={{
+          off: { width: 0, display: 'none' },
+          on: { width: 80, display: 'flex' },
+        }}
+      >
+        {subMenuData &&
+          subMenuData.map((item, i) => (
+            <Link href={item.link}>
+              <div className='text-zinc-300 duration-300 hover:text-white rotate-180'>
+                {item.name}
+              </div>
+            </Link>
+          ))}
+      </motion.div>
 
       {/* MOBILE NAV */}
       <NavbarMobile />
