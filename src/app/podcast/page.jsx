1e71141f4c532f6c462a04f59b54b532a285bf66
Vitor Butkus <vitorbutkus@gmail.com>
2024-07-16 16:00:52 -0300

    add RevealText in podcast page

--- src/app/podcast/page.jsx
+++ src/app/podcast/page.jsx
@@ -2,6 +2,7 @@ import { load } from 'outstatic/server';
 import markdownToHtml from '../../lib/markdownToHtml';
 import ItemCard from '@/components/ItemCard';
 import HeaderSubpage from '@/components/HeaderSubpage';
+import RevealText from '@/components/RevealText';
 // import OutstaticSchema from 'outstatic/dist/utils/server';
 // import imgPlaceholder from '@/public/images/imgPlaceholder.webp';
 
@@ -37,8 +38,54 @@ export default async function Page() {
       {/* Header */}
       <HeaderSubpage title='Podcast' bgImg='/images/dummyImg.png' />
 
+      {/* Intro text */}
+      <div className='pt-24 pb-16 text-white/90 bg-zinc-900 w-full flex flex-col items-center'>
+        <div className='w-full max-w-[90%] md:max-w-[700px] lg:max-w-[900px] flex flex-col items-center justify-center gap-8 text-white'>
+          <RevealText>
+            <div className=''>
+              <span className='font-bold'>Arte Sonora Podcast</span> apresenta
+              de forma monográfica os principais artistas e pensadores
+              brasileiros que tem investigando o mundo através do som e suas
+              interfaces,apresentando seu pensamento, obras e interesses.
+            </div>
+          </RevealText>
+          <RevealText>
+            <div className=''>
+              O programa foi estruturado na forma de uma conversa informal, e
+              algumas das vezes foi gravado no próprio espaço do entrevistado,
+              exceto um depoimento e duas palestras que ocorreram na Escola de
+              Artes Visuais do Parque Lage.
+            </div>
+          </RevealText>
+          <RevealText>
+            <div className=''>
+              Procuramos colocar o artista e seu pensamento no centro do
+              problema. Geralmente, conversamos sobre formação, o interesse pela
+              arte e o som, passando pelos principais trabalhos e mostras. Ao
+              final, cada entrevistado é instigado a refletir sobre o que lhe
+              move nos dias de hoje.
+            </div>
+          </RevealText>
+          <RevealText>
+            <div className=''>
+              Nossas escolhas são de caráter afetivo mas não deixam de
+              considerar o percurso, relevância histórica e originalidade do
+              trabalho.{' '}
+            </div>
+          </RevealText>
+          <RevealText>
+            <div className=''>
+              Por fim, entendemos Arte Sonora como um campo amplo e plural que
+              recusa categorizações, o que nos permite apresentar artistas,
+              pesquisadores, acadêmicos, músicos, maestros, DJs produtores,
+              repentistas e muito mais.
+            </div>
+          </RevealText>
+        </div>
+      </div>
+
       {/* Content */}
-      <div className='pt-10 pb-60 bg-zinc-900 w-full min-h-screen flex flex-col justify-center items-center'>
+      <div className='pb-60 bg-zinc-900 w-full min-h-screen flex flex-col justify-center items-center'>
         <div className='w-full max-w-[90%] md:max-w-[700px] lg:max-w-[900px] flex flex-col items-center justify-center gap-8 text-white'>
           {posts.map((post, i) => (
             <>
