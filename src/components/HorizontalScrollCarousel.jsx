9e194450d96c039de99a7fba46aa3b30e1b50489
Vitor Butkus <vitorbutkus@gmail.com>
2024-07-18 19:10:15 -0300

    fix HorizontalScrollCarousel width

--- src/components/HorizontalScrollCarousel.jsx
+++ src/components/HorizontalScrollCarousel.jsx
@@ -17,8 +17,11 @@ const HorizontalScrollCarousel = ({ newestPosts }) => {
     : useTransform(scrollYProgress, [0, 1], ['0.5%', '-95%']);
 
   return (
-    <section ref={targetRef} className='relative h-[300vh] bg-neutral-900'>
-      <div className='sticky top-0 flex  h-[calc(100vh)] md:h-[calc(100vh-84px)] items-center overflow-hidden'>
+    <section
+      ref={targetRef}
+      className='relative md:w-[calc(100vw-52px)] h-[300vh] bg-neutral-900'
+    >
+      <div className='sticky top-0 flex w-full h-[calc(100vh)] md:h-[calc(100vh-84px)] items-center overflow-hidden'>
         <div className='absolute top-20 md:top-6 left-6 mb-6 text-6xl md:text-7xl pr-10 w-full text-right text-white/10'>
           EM DESTAQUE
         </div>
