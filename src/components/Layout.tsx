0ed32c7baca8d2157dc34f02da3884cbe26708d1
Vitor Butkus <vitorbutkus@gmail.com>
2024-07-22 16:07:42 -0300

    add syncTouchLerp to 0 in Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -23,6 +23,7 @@ const Layout = ({ children }: Props) => {
     const lenis = new Lenis({
       smoothWheel: true,
       syncTouch: true,
+      syncTouchLerp: 0,
     });
 
     function raf(time: number) {
