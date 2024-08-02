1bc735335600af8ec4305afe8718ee2640767b2a
Vitor Butkus <vitorbutkus@gmail.com>
2024-08-02 19:59:06 -0300

    change color of title in Hero to brighter one

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -46,7 +46,7 @@ export default function Hero() {
       </div>
 
       {/* title */}
-      <div className='absolute right-auto top-1/2 left-1/2 -translate-x-1/2 md:translate-x-0 -translate-y-1/2 md:translate-y-0 md:left-auto md:top-5 md:right-5 tracking-widest text-7xl md:text-8xl text-neutral-700 text-center md:text-right font-chakra font-semibold'>
+      <div className='absolute right-auto top-1/2 left-1/2 -translate-x-1/2 md:translate-x-0 -translate-y-1/2 md:translate-y-0 md:left-auto md:top-5 md:right-5 tracking-widest text-7xl md:text-8xl text-neutral-600 text-center md:text-right font-chakra font-semibold'>
         <motion.div
           initial='off'
           animate='on'
@@ -60,7 +60,7 @@ export default function Hero() {
       </div>
 
       {/* text */}
-      <div className='absolute top-5 md:top-auto bottom-auto md:bottom-5 md:left-5 text-white/80 w-3/4 text-left md:text-left md:pb-0 px-5 md:px-0 md:w-1/3 text-sm md:text-base'>
+      <div className='absolute top-5 md:top-auto bottom-auto md:bottom-5 md:left-5 text-white/90 w-3/4 text-left md:text-left md:pb-0 px-5 md:px-0 md:w-1/3 text-sm md:text-base'>
         Arte Sonora é uma prática artística coletiva desenvolvida pelo duo de
         artistas <span className='font-bold'>Franz Manata</span> e{' '}
         <span className='font-bold'>Saulo Laudares</span>, através de cursos,
@@ -71,7 +71,7 @@ export default function Hero() {
 
       {/* realização */}
       <motion.div
-        className='absolute right-5 bottom-5 text-white/80 text-base md:text-lg tracking-wider font-chakra -translate-y-[55px] md:translate-y-0'
+        className='absolute right-5 bottom-5 text-white/50 text-base md:text-lg tracking-wider font-chakra -translate-y-[55px] md:translate-y-0'
         initial='off'
         animate='on'
         transition={{ delay: 0.3 }}
